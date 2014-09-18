3957753b6bc10dd606b0d4c01206faae73b895ab
david3424 <david.3424@gmail.com>
2014-09-18 14:15:27 +0800

    modify kpi bean

--- src/main/java/org/conan/myhadoop/mr/kpi/KPIBrowser.java
+++ src/main/java/org/conan/myhadoop/mr/kpi/KPIBrowser.java
@@ -4,74 +4,68 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 public class KPIBrowser {
 
-    public static class KPIBrowserMapper extends MapReduceBase implements Mapper<Object, Text, Text, IntWritable> {
+    public static class KPIBrowserMapper extends Mapper<Object, Text, Text, IntWritable> {
         private IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        @Override
-        public void map(Object key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             KPI kpi = KPI.filterBroswer(value.toString());
             if (kpi.isValid()) {
                 word.set(kpi.getHttp_user_agent());
-                output.collect(word, one);
+                context.write(word, one);
             }
         }
     }
 
-    public static class KPIBrowserReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
+    public static class KPIBrowserReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
         @Override
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
-            while (values.hasNext()) {
-                sum += values.next().get();
+            for (IntWritable val : values) {
+                sum += val.get();
             }
-            result.set(sum);
-            output.collect(key, result);
+            context.write(key, new IntWritable(sum));
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration _conf = new Configuration();
         String otherArgs[] = (new GenericOptionsParser(_conf, args)).getRemainingArgs();
-        if (otherArgs.length != 2)
-        {
+        if (otherArgs.length != 2) {
             System.err.println("Usage: org.conan.myhadoop.mr.kpi.KPIBrowser <in> <out>");
             System.exit(2);
         }
 
         String input = otherArgs[0];
-        String output = otherArgs[1] ;
-        JobConf conf = new JobConf(KPIBrowser.class);
-        conf.setJobName("KPIBrowser");
-        conf.addResource("classpath:/hadoop/core-site.xml");
+        String output = otherArgs[1];
+        Job job = new Job(_conf, "KPIBrowser");
+        job.setJarByClass(KPIBrowser.class);
+        job.setMapperClass(KPIBrowserMapper.class);
+        job.setReducerClass(KPIBrowserReducer.class);
+        job.setCombinerClass(KPIBrowserReducer.class);//对数据进行早期聚合，结果不再是(is,1,1,1) 而是(is,3) 不能简单的+1，得求和
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setJobName("KPIBrowser");
+      /*  conf.addResource("classpath:/hadoop/core-site.xml");
         conf.addResource("classpath:/hadoop/hdfs-site.xml");
-        conf.addResource("classpath:/hadoop/mapred-site.xml");
-        
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(IntWritable.class);
+        conf.addResource("classpath:/hadoop/mapred-site.xml");*/
+        FileInputFormat.setInputPaths(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
 
-        conf.setMapperClass(KPIBrowserMapper.class);
-        conf.setCombinerClass(KPIBrowserReducer.class);
-        conf.setReducerClass(KPIBrowserReducer.class);
-
-        conf.setInputFormat(TextInputFormat.class);
-        conf.setOutputFormat(TextOutputFormat.class);
-
-        FileInputFormat.setInputPaths(conf, new Path(input));
-        FileOutputFormat.setOutputPath(conf, new Path(output));
-
-        JobClient.runJob(conf);
-        System.exit(0);
+//        JobClient.runJob(conf);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
 }
